1d340a76a5d7d03a578089e62195ec80a3d34ee1
Isard <2023_talia.lopez@iticbcn.cat>
2024-05-14 19:29:03 +0200

    Objetos actualizados

---
+++ Objetos/Categoria.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class Category(BaseModel):
+    category_id: int = None
+    name: str
+    created_at: datetime = None
+    updated_at: datetime = None
---
+++ Objetos/Producto.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Product(BaseModel):
+    product_id: int = None  # Opcional para nuevos productos, autoincrementado por la DB.
+    name: str
+    description: str
+    company: str
+    price: float
+    units: int
+    subcategory_id: int
+    created_at: datetime = None
+    updated_at: datetime = None
