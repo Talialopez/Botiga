42632be33ccfc9007d917e84b07494e2581b8668
ramon <2023_ramon.font@iticbcn.cat>
2024-05-15 16:50:01 +0200

    Comentaris afegits

--- Objetos/Categoria.py
+++ Objetos/Categoria.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+# Definición de la clase Category que hereda de BaseModel
 class Category(BaseModel):
     category_id: int = None
     name: str
     created_at: datetime = None
     updated_at: datetime = None
 
+# Función para convertir un objeto de categoría en un diccionario
 def category_schema(category) -> dict:
     return {
         "category_id": category["category_id"],
@@ -15,5 +17,6 @@ def category_schema(category) -> dict:
         "updated_at": category["updated_at"]
     }
 
+# Función para convertir una lista de objetos de categoría en una lista de diccionarios
 def categories_schema(categories) -> list:
     return [category_schema(category) for category in categories]
